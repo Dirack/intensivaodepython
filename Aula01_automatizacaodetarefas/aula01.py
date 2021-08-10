bed8e4bd32b4bcc7a5c3314b47f88e9cc7a05e76
Dirack <rodolfo_profissional@hotmail.com>
2021-08-09 22:10:58 -0300

    tabela

--- Aula01_automatizacaodetarefas/aula01.py
+++ Aula01_automatizacaodetarefas/aula01.py
@@ -1,6 +1,7 @@
 import pyautogui as pya
 import pyperclip as pc
 import time
+import pandas as pd
 
 pya.PAUSE = 3
 pya.hotkey("win","8")
@@ -15,3 +16,7 @@ pya.click(x=455,y=439)
 pya.click(x=1713,y=223)
 pya.click(x=1617,y=654)
 
+time.sleep(5)
+
+tabela = pd.read_excel(r'~/Downloads/Vendas - Dez.xlsx')
+print(tabela)
